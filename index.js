39f45f5625d4e555f05f3cb730ca948bfeb1f100
nathanielhowe97 <130931122+nathanielhowe97@users.noreply.github.com>
2023-10-10 21:30:55 -0500

    upload files

---
+++ index.js
@@ -0,0 +1,56 @@
+const noteInputTA = document.querySelector('#create-note')
+const submitNoteBtn = document.querySelector('#submit-note-btn')
+const pastNotesList = document.querySelector('#past-note-list')
+
+const toast = document.querySelector('#toast')
+
+const remove = document.querySelector('.remove')
+
+
+
+submitNoteBtn.addEventListener('click', function(){
+
+    if (noteInputTA.value === ""){
+        showToast()
+    } else{
+        let pastNotes = document.createElement('li')
+        pastNotes.textContent = noteInputTA.value
+        pastNotesList.appendChild(pastNotes)
+
+        let removeNote = document.createElement("button")
+        removeNote.innerHTML = `<span class="material-symbols-outlined">delete</span>`
+        pastNotes.appendChild(removeNote)
+
+        noteInputTA.value = ""
+        SaveLS()
+    }
+
+    
+})
+
+pastNotesList.addEventListener('click', function(e){
+    let clickTarget = e.target
+
+    if (clickTarget.tagName === "SPAN"){
+        clickTarget.parentElement.parentElement.remove()
+        SaveLS()
+    }
+})
+
+function showToast(){
+    toast.style.display = "block"
+
+    setTimeout(() => {
+        toast.style.display = "none"
+    }, 5000);
+}
+
+function SaveLS(){
+    localStorage.setItem('pastNotes', pastNotesList.innerHTML)
+}
+
+function RenderLS(){
+    pastNotesList.innerHTML = localStorage.getItem('pastNotes')
+}
+
+RenderLS()
